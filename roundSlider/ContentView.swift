82b44d4dc462a76eab9c7e792ee7e8019b883b54
ilmukov <30494462+ilmukov@users.noreply.github.com>
2021-07-25 20:17:13 +0300

    Initial Commit

---
+++ roundSlider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  roundSlider
+//
+//  Created by Stepan Ilmukov on 25.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ roundSlider/roundSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  roundSliderApp.swift
+//  roundSlider
+//
+//  Created by Stepan Ilmukov on 25.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct roundSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
